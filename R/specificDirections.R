10ae079dd39749ba48088d044ddeaa5b0f890813
sagesteppe <reedbenkendorf27@gmail.com>
2023-06-28 13:37:37 -0700

    reminder never regex html, more subs for directions, three fns to tie together for directions_grabber

--- R/specificDirections.R
+++ R/specificDirections.R
@@ -1,11 +1,11 @@
 
 #' Import Google directions to a site
-#' 
-#' Use Google Maps to write directions to a site, so that the people re-locating 
+#'
+#' Use Google Maps to write directions to a site, so that the people re-locating
 #' the site don't need to use Google Maps to write directions to a site. These
-#' are crude and will require some refinement for some relevant applications. 
+#' are crude and will require some refinement for some relevant applications.
 #' @param x the output of directions_grabber
-#' @example 
+#' @example
 #' SoS_gkey = Sys.getenv("Sos_gkey")
 #' df <- google_directions(origin = "Reno, Nevada",
 #"                        destination = "39.2558, -117.6827",
@@ -13,46 +13,67 @@
 #'                        mode = "driving",
 #'                        simplify = TRUE)
 #' specificDirections(df)
-#' @export 
+#' @export
 specificDirections <- function(x){
-  
+
+  strip_html <- function(x) {
+    rvest::html_text(rvest::read_html(charToRaw(x)))
+  }
+
   cleanFun <- function(x) {
-    y <- gsub("<.*?>", "", x)
-    y <- gsub('Pass by Burger King.*$', "", y)
+    y <- gsub('Pass by Burger King.*$', "", x)
     y <- gsub('Destination will be on.*$', "", y)
     y <- gsub('onto', 'on', y)
     y <- gsub('Continue', "Con't", y)
     y <- gsub('continue', "con't", y)
+    y <- gsub( " [A-z].*Con't", " Con't", y)
     y <- gsub(' and becomes .*$', '', y)
     y <- gsub(' right ', ' R ', y)
     y <- gsub(' left ', ' L ', y)
     y <- gsub('BUS', '', y)
+    y <- gsub("Turn Con't", "Con't", y)
     y <- gsub('ALT', 'Alt', y)
+    y <- gsub("Con't Con't", "Con't", y)
     return(y)
   }
 
   dirStartEnd <- function(x){
     y <- gsub('^Continue onto ', 'Take ', x)
     y <- gsub('^Take the ramp onto ', 'Take ', x)
+    y <- gsub("^Head Con't to follow", 'Follow', x)
     y <- trimws(y)
     return(y)
   }
-  
-  dist <- df[['routes']][['legs']][[1]][['steps']][[1]]$distance
+
+  dist <- x[['routes']][['legs']][[1]][['steps']][[1]]$distance
   dist <- data.frame(
-    distance = dist$value, 
-    t_dist = sum(dist['value'] ), 
+    distance = dist$value,
+    t_dist = sum(dist['value'] ),
     c_dist = cumsum(dist['value']) / sum(dist['value']) * 100,
     dist_mi = round(dist$value * 0.000621371, 1)
   )
-  distances <- dist[ which(dist$value > 1), ]
 
-  instruct <- df[['routes']][['legs']][[1]][['steps']][[1]]$html_instructions
-  instruct <- instruct[ which(dist$value > 1) ]
-  instruct <- cleanFun(instruct) # need to pad destination. 
-  
-  directionsString <- paste(instruct, distances$dist_mi, 'mi. ', collapse = "") 
+  if(nrow(dist) > 5 ){
+    distances <- dist[which(dist$value > 1), ]
+  } else {
+    distances <- dist
+  }
+
+  instruct <- x[['routes']][['legs']][[1]][['steps']][[1]]$html_instructions
+  if(nrow(dist) > 5 ){
+    instruct <- instruct[which(dist$value > 1) ]
+  } else {
+    instruct <- instruct
+  }
+
+  instruct <- vapply(instruct, FUN = strip_html, FUN.VALUE = 'character')
+  instruct <- cleanFun(instruct) # need to pad destination.
+
+  directionsString <- paste(instruct, distances$dist_mi, 'mi. ')
+  directionsString <- paste0(directionsString, '. ',  collapse = "")
+  directionsString <- gsub('. .', '.', directionsString, fixed = TRUE)
   directionsString <- dirStartEnd(directionsString)
-  
+  directionsString <- gsub(" 0 mi", "", directionsString)
+
   return(directionsString)
 }
