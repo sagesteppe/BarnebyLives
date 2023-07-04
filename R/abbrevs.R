e64d362f0e44cbefc568b170f148df1016baebdb
sagesteppe <reedbenkendorf27@gmail.com>
2023-07-04 11:08:32 -0700

    author names

---
+++ R/abbrevs.R
@@ -0,0 +1,11 @@
+#' abbrevs
+#'
+#' This is a reproduced list of IPNI botanical authors
+#'
+#' This data set is a vector of International Plant Names Index (IPNI) standard abbreviations for botanical authors, and includes a space between the given and surnames. 
+#'
+#' @format vector
+#' @examples
+#' head(abbrevs)
+"abbrevs"
+
