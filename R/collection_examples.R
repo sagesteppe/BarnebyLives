1ed02ba1fec3396485b5e16245ffe1144e95e834
sagesteppe <reedbenkendorf27@gmail.com>
2023-07-07 15:30:37 -0700

    outline to generate labels

---
+++ R/collection_examples.R
@@ -0,0 +1,12 @@
+#' collection_examples
+#'
+#' This is a small data frame of species information for use with the shipping/transferral notes
+#' as well as with the label makers.
+#'
+#' this data frame is derived from collections in 2023, which have been cleaned with the
+#' BarnebyLives pipeline
+#'
+#' @format A data frame with some collections which will one day be shipped.
+#' @examples
+#' head(collection_examples)
+"collection_examples"
