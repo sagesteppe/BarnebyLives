b2d6ef917849d76458dc3d1c75f4bdc6bcbbfffe
sagesteppe <reedbenkendorf27@gmail.com>
2023-08-30 14:31:48 -0700

    data usage

--- data-raw/sample_data.R
+++ data-raw/sample_data.R
@@ -6,4 +6,4 @@ googledrive::drive_auth("reedbenkendorf27@gmail.com")
 # read in data from the sheet to process
 collection_examples <- read_sheet('1iOQBNeGqRJ3yhA-Sujas3xZ2Aw5rFkktUKv3N_e4o8M',
                         sheet = 'Processed - Examples')
-usethis::use_data(collection_examples)
+usethis::use_data(collection_examples, overwrite = TRUE)
