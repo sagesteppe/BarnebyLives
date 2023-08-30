4691006c091dbaaa009db05e79e5a5c52925a013
sagesteppe <reedbenkendorf27@gmail.com>
2023-08-30 13:51:12 -0700

    example data updated

--- data-raw/sample_data.R
+++ data-raw/sample_data.R
@@ -1,8 +1,9 @@
 library(usethis)
 library(googlesheets4)
 
+setwd('/media/steppe/hdd/BarnebyLives/data-raw')
 googledrive::drive_auth("reedbenkendorf27@gmail.com")
 # read in data from the sheet to process
 collection_examples <- read_sheet('1iOQBNeGqRJ3yhA-Sujas3xZ2Aw5rFkktUKv3N_e4o8M',
                         sheet = 'Processed - Examples')
-usethis::use_data(processed)
+usethis::use_data(collection_examples)
