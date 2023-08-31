3a333b8d8a2f6ed339b297990e749d0dc18f22e7
sagesteppe <reedbenkendorf27@gmail.com>
2023-08-31 14:22:39 -0700

    collection writer typos ... reduced

--- R/collection_writer.R
+++ R/collection_writer.R
@@ -3,13 +3,14 @@
 #' Format these data to have commas or periods as appropriate
 #' @param x dataframe holding the values
 #' @export
-collection_write <- function(x){
+collection_writer <- function(x){
 
   x$Associated_Collectors[x$Associated_Collectors==""] <- NA
 
   if (is.na(x$Associated_Collectors)){
     paste0(data$Primary_Collector, data$Collection_number, ',', data$Date_digital_text, '.')
   } else {
-    paste0(paste0(data$Primary_Collector, data$Collection_number, ',',
-                  data$Associated_Collectors, ',', data$Date_digital_text, '.')}
+    paste0(data$Primary_Collector, data$Collection_number, ',',
+                  data$Associated_Collectors, ',', data$Date_digital_text, '.')
+           }
 }
