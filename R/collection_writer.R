3da0f98f6400ab43ade6693fe360a94079a74656
sagesteppe <reedbenkendorf27@gmail.com>
2023-08-31 16:22:34 -0700

    updated sample data

--- R/collection_writer.R
+++ R/collection_writer.R
@@ -2,15 +2,20 @@
 #'
 #' Format these data to have commas or periods as appropriate
 #' @param x dataframe holding the values
+#' @example
+#' data('collection_examples')
+#' ce <- collection_examples[14,]
+#' collection_writer(ce)
 #' @export
 collection_writer <- function(x){
 
   x$Associated_Collectors[x$Associated_Collectors==""] <- NA
 
   if (is.na(x$Associated_Collectors)){
-    paste0(x$Primary_Collector, x$Collection_number, ',', x$Date_digital_text, '.')
+    paste0(x$Primary_Collector, " ", x$Collection_number, '; ', x$Date_digital_text)
   } else {
-    paste0(x$Primary_Collector, x$Collection_number, ',',
-                  x$Associated_Collectors, ',', x$Date_digital_text, '.')
+    paste0(x$Primary_Collector, " ",  x$Collection_number, ', ',
+                  x$Associated_Collectors, '; ', x$Date_digital_text)
            }
 }
+
