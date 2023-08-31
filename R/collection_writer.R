f015a6e53a3fc62aefaa58096908e575f6b26081
sagesteppe <reedbenkendorf27@gmail.com>
2023-08-31 14:17:11 -0700

    fn to write out collector info

---
+++ R/collection_writer.R
@@ -0,0 +1,15 @@
+#' format collector info and codes
+#'
+#' Format these data to have commas or periods as appropriate
+#' @param x dataframe holding the values
+#' @export
+collection_write <- function(x){
+
+  x$Associated_Collectors[x$Associated_Collectors==""] <- NA
+
+  if (is.na(x$Associated_Collectors)){
+    paste0(data$Primary_Collector, data$Collection_number, ',' data$Date_digital_text, '.')
+  } else {
+    paste0(paste0(data$Primary_Collector, data$Collection_number ','
+                  data$Associated_Collectors, ',' data$Date_digital_text, '.')}
+}
--- R/writer_fide.R
+++ R/writer_fide.R
@@ -2,7 +2,7 @@
 #'
 #' This functions determines whether to write information on whom determined the specimen
 #' when, and via which texts. It used as a final step when formatting labels.
-#' @param x
+#' @param x dataframe holding the values
 #' @examples
 #' phacelia <- collection_examples[31,]
 #' writer_fide(phacelia)
@@ -14,7 +14,7 @@ writer_fide <- function(x){
   if (is.na(x$Fide)){""} else {
     paste0('Fide: *',
            sub(' Flora ', ' Fl. ', x$Fide),
-           '*, by: ', x$Determined_by, ' on ', x$Determined_date_text)}
+           '*, by: ', x$Determined_by, ' on ', x$Determined_date_text), '.'}
 }
 
 
