8e1bba205291edfd45bcd196e8c4313688b7b99a
sagesteppe <reedbenkendorf27@gmail.com>
2023-09-01 09:59:14 -0700

    veg do not end with ..

--- R/species_font.R
+++ R/species_font.R
@@ -1,30 +1,31 @@
 #' ensure proper italicization of a associated species
-#' 
+#'
 #' This function works at the time of label creation to ensure that abbreviations
 #' such as 'species' 'variety' and such are not italicizded, per convention.
 #' @param x a value being feed into the label during the merge
-#' @examples 
+#' @examples
 #' associates <- c('Eriogonum ovalifolium var. purpureum, Castilleja sp., Crepis spp.')
 #' spp_normal(associates)
 #' @export
 species_font <- function(x){
-   
+
   x <- paste0('*', x, '*')
-  
+
   if(any(grep(' sp..| s.p[.]| var. | subsp. ', x)) == TRUE){
     x <- gsub(' sp[.], ', '* sp., *', x)
     x <- gsub(' sp[.]\\*$', '* sp.', x)
     x <- gsub(' spp[.]\\*$', '* spp.', x)
     x <- gsub(' spp[.], ', '* spp., *', x)
-    
+
     # infraspecies will always have a space before and after - WORKING
     x <- gsub(' ssp[.] ', '* ssp. *', x)
     x <- gsub(' ssp[.]\\*$', '* ssp.', x)
     x <- gsub(' var[.] ', '* var. *', x)
     x <- gsub(' subsp[.] ', '* spp. *', x)
     return(x)
-  } 
-  
+  }
+
   x <- gsub('  ', ' ', x)
+  x <- gsub("..", ".", x, fixed = TRUE)
   return(x)
 }
