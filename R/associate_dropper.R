164432c31c42ca32666153347f6598c70b7e465d
sagesteppe <reedbenkendorf27@gmail.com>
2023-09-11 13:54:51 -0700

    reduced associate dropper to operate on binomial and not infraspecies

--- R/associate_dropper.R
+++ R/associate_dropper.R
@@ -4,8 +4,7 @@
 #' species
 #' @param x a dataframe containing collection info
 #' @param Binomial a column containing the name of the collection, without authorship.
-#' @example
-#'
+#' @examples
 #' out <- associate_dropper(collection_examples, Binomial = Full_name) |>
 #'  dplyr::select(Full_name, Associates)
 #' @export
@@ -17,7 +16,7 @@ associate_dropper <- function(x, Binomial){
   x <- x |>
     dplyr::rowwise() |>
     dplyr::mutate(
-      Associates = gsub(paste0(Binomial), "", Associates),
+      Associates = gsub(stringr::word(Binomial, 1,2, sep=" "), "", Associates),
       Associates = trimws(gsub("\\s+", " ", Associates)),
       Associates = gsub(",$", "", Associates),
       Associates = gsub("^, ", "", Associates),
