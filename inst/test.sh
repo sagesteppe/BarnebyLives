8f4ac03d505c75711e5c16e11bc36da6512ad20e
sagesteppe <reedbenkendorf27@gmail.com>
2024-08-06 16:28:38 -0600

    working onfeeding filesin

--- inst/test.sh
+++ inst/test.sh
@@ -8,18 +8,10 @@ for arg in "$@"; do
 done
 
 mkdir ${collector}-processed
+
 files=$(find ${collector}-raw -mindepth 1 | sort -Vt / -k2,2)
 for (( i=0; i<${#files[*]}; i+=4 ));
 do
   filename="${files[i]##*/}"
   pdfjam "${files[@]:$i:4}" --nup 2x2 --landscape --outfile ${collector}-processed/$filename --noautoscale true --quiet
 done
-
-mkdir ../final
-files=$(find ${collector}-processed | sort -Vt / -k2,2)
-pdftk ${files[*]} output ../final/${collector}-labels.pdf
-
-echo "final labels are located at: final/${collector}-labels.pdf"
-
-#rm ${collector}-raw -r
-rm ${collector}-processed -r
