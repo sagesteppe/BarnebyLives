86010f8eea4f58ad384c56be889cab7bafbe1bf4
sagesteppe <reedbenkendorf27@gmail.com>
2024-08-06 17:19:50 -0600

    bash script to combine labels

--- inst/render_labels.sh
+++ inst/render_labels.sh
@@ -8,7 +8,7 @@ for arg in "$@"; do
 done
 
 mkdir ${collector}-processed
-files=$(find ${collector}-raw -mindepth 1 | sort -Vt / -k2,2)
+files=($(ls ${collector}-raw/* | sort -Vt / -k2,2))
 for (( i=0; i<${#files[*]}; i+=4 ));
 do
   filename="${files[i]##*/}"
@@ -16,10 +16,10 @@ do
 done
 
 mkdir ../final
-files=$(find ${collector}-processed | sort -Vt / -k2,2)
+files=($(ls ${collector}-processed/* | sort -Vt / -k2,2))
 pdftk ${files[*]} output ../final/${collector}-labels.pdf
 
 echo "final labels are located at: final/${collector}-labels.pdf"
 
-#rm ${collector}-raw -r
+rm ${collector}-raw -r
 rm ${collector}-processed -r
