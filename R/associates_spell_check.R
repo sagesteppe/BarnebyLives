997cce1f217944464c2abfa58a79b343f5dac08c
sagesteppe <reedbenkendorf27@gmail.com>
2024-08-07 16:10:46 -0600

    spell check push

--- R/associates_spell_check.R
+++ R/associates_spell_check.R
@@ -15,14 +15,16 @@
 #' associates_spell_check.R(x = df, column = 'Associates', p2tax)
 #' }
 #' @export
-associates_spell_check.R <- function(x, column, path){
+associates_spell_check <- function(x, column, path){
 
   if(missing(column)){column <- 'Associates' ;
     message(crayon::yellow('Argument to `column` not supplied, defaulting to `Associates`'))}
 
   # remove notation of '.sp' and simply search that the genus is spelled correctly.
   # we'll simply add 'sp.' to any single piece returned by the fn.
-  associates <- gsub(' sp.', '', x[,column])
+  associates <- dplyr::pull(x, column)
+
+  associates <- trimws(associates)
 
   # import look up table
   sppLKPtab <- read.csv(file.path(path, 'species_lookup_table.csv'))
@@ -44,11 +46,14 @@ associates_spell_check.R <- function(x, column, path){
 
       if(L == 1){ # genus search
 
-        genLKPsub <- genLKPtab[
-          grepl(substr(names2search[[i]], 1, 2), genLKPtab$gGRP, ignore.case = TRUE),]
-        names2search[[i]] <- paste(genLKPsub[
-          which.min(
-            stringdist::stringdist(names2search[[i]], genLKPsub$genus, method = 'jw')), 'genus'], 'sp.')
+        if(is.na(names2search[[i]])){names2search[[i]] <- names2search} else {
+
+          genLKPsub <- genLKPtab[
+            grepl(substr(names2search[[i]], 1, 2), genLKPtab$gGRP, ignore.case = TRUE),]
+          names2search[[i]] <- paste(genLKPsub[
+            which.min(
+              stringdist::stringdist(names2search[[i]], genLKPsub$genus, method = 'jw')), 'genus'], 'sp.')
+        }
 
       } else if(L == 2){ # species search
 
@@ -61,15 +66,30 @@ associates_spell_check.R <- function(x, column, path){
         } else if(L == 4) { # infra species search
 
           infra_sppLKPsub <- dplyr::filter(infra_sppLKPtab, gGRP == substr(names2search[[i]], 1, 2))
-          names2search[[i]] <- infra_sppLKPsub$taxon_name[
+          names2search[[i]] <- infra_sppLKPsub[
             which.min(
-              stringdist::stringdist(names2search[i], infra_sppLKPsub$taxon_name, method = 'jw')), 'taxon_name'
+              stringdist::stringdist(names2search[[i]], infra_sppLKPsub$taxon_name, method = 'jw')), 'taxon_name']
 
-          ]}
+          }
     }
     associates[[y]] <- paste(names2search, collapse = ', ')
   }
 
   spellck <- setNames(data.frame(associates), paste0(column, '-SpellCk'))
-  data.frame(cbind(x, spellck))
+  out <- dplyr::bind_cols(x, spellck)
+  return(out)
 }
+
+
+df <- data.frame(
+  named = c(
+    NA,
+    NA,
+  'Boudelou gracilis, Bromus Tectorum, Opuntia sp., vulpia octoflora, Artimisia sp., Taeniatherum caput-medusae',
+  'Coleogyne racimosissima',
+  'Bromus tetorum, Eremoporum triticum, sarcbatus vermiculatus')
+)
+
+
+p2tax <- '/media/steppe/hdd/Barneby_Lives-dev/taxonomic_data'
+associates_spell_check(x = df, column = 'named', p2tax)
