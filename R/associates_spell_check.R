e6514fe8d574ecb512fe14365188a3bfbe943ea9
sagesteppe <reedbenkendorf27@gmail.com>
2024-08-23 15:37:12 -0600

    Associates spell check now incorporates overlooked search for spp.

--- R/associates_spell_check.R
+++ R/associates_spell_check.R
@@ -19,9 +19,8 @@ associates_spell_check <- function(x, column, path){
     message(crayon::yellow('Argument to `column` not supplied, defaulting to `Associates`'))}
 
   # remove notation of '.sp' and simply search that the genus is spelled correctly.
-  # we'll simply add 'sp.' to any single piece returned by the fn.
+  # we'll simply add 'sp.' to any single piece returned by the fn
   associates <- dplyr::pull(x, column)
-
   associates <- trimws(associates)
 
   # import look up table
@@ -37,20 +36,31 @@ associates_spell_check <- function(x, column, path){
     # separate each name into an element of a vector
     names2search <- unlist(stringr::str_split(associates[[y]], pattern = ","))
     names2search <- trimws(names2search)
+    uncertainty <- vector(mode= 'character', length = length(names2search))
 
     for (i in seq_along(names2search)){
+
+      # determine which entries are not identified to species, only search the
+      # spell check functionality on the generic moniker.
+      if(grepl(' spp[.]', names2search[i])) {
+        uncertainty[i] <- 'spp.'
+        names2search[i] <- gsub(' spp[.]', '', names2search[i])
+      } else if(grepl(' sp[.]', names2search[i])){
+        uncertainty[i] <- 'sp.'
+        names2search[i] <- gsub(' sp[.]', '', names2search[i])
+      } else {uncertainty[i] <- NA}
 
       L = lengths(strsplit(names2search[i], " "))
 
       if(L == 1){ # genus search
 
-        if(is.na(names2search[[i]])){names2search[[i]] <- names2search} else {
+        if(is.na(names2search[[i]])){names2search[[i]] <- names2search[[i]]} else {
 
           genLKPsub <- genLKPtab[
             grepl(substr(names2search[[i]], 1, 2), genLKPtab$gGRP, ignore.case = TRUE),]
           names2search[[i]] <- paste(genLKPsub[
             which.min(
-              stringdist::stringdist(names2search[[i]], genLKPsub$genus, method = 'jw')), 'genus'], 'sp.')
+              stringdist::stringdist(names2search[[i]], genLKPsub$genus, method = 'jw')), 'genus'], uncertainty[[i]])
         }
 
       } else if(L == 2){ # species search
