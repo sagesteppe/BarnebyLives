f542368eae6946e72659549131e261c654145d6c
sagesteppe <reedbenkendorf27@gmail.com>
2024-12-29 18:02:32 -0600

    badges

--- R/contacts_grabber.R
+++ R/contacts_grabber.R
@@ -11,22 +11,34 @@ museums$address[, grep('physical|postal', colnames(museums$address)) ]
 
 contacts_grabber <- function(x){
 
+  # CAT THE CODE WE ARE SEARCHING FOR HERE!!! NO NEED TO DL THE WHOLE COUNTRY TO GET
+  # THE RELEVANT RESULTS, START OUT SUBSET...
+
   url <- "http://sweetgum.nybg.org/science/api/v1/institutions/search?country=U.S.A."
   museums <- jsonlite::fromJSON(url)$data
 
   museums <- cbind(
-    museumsTEST <- museums[,c('organization', 'code')], # these two fields from 1st table
 
-    { musemsTEST2 <- museums$address[, grep('physical|postal', colnames(museums$address)) ]
-      musemsTEST2[musemsTEST2==""] <- NA # it's easy to test for NA's
-      museumsTEST2 <- dplyr::mutate(musemsTEST2,
-          postalStreet = ifelse(is.na(postalStreet), physicalStreet, postalStreet),
-          postalCity = ifelse(is.na(postalCity), physicalCity, postalCity),
-          postalState = ifelse(is.na(postalState), physicalState, postalState),
-          postalZipCode = ifelse(is.na(postalZipCode), physicalZipCode, postalZipCode),
-          postalCountry = 'U.S.A.' # yeah some places are missing this...
-        )
-    museumsTEST2 <- museumsTEST2[, grep('postal', colnames(museumsTEST2))]},
+    museums[,c('organization', 'code')], # these two fields from 1st table
+
+    { # here we grab the shipping addresses.
+      museum_addy <- museums$address[, grep('physical|postal', colnames(museums$address)) ]
+      museum_addy[museum_addy==""] <- NA # it's easy to test for NA's
+
+      # overwrite original values that are NA, with the physical address information.
+      # really long winded- but want to be able to deal with any IH name changes
+      # without having to refactor.
+      post <- cnames[grep('postal', cnames)]
+      for (i in seq_along(post)){
+        museum_addy[,museum_addy[i]] <- ifelse(
+          is.na(
+            museum_addy[,museum_addy[i]]),
+          museum_addy[, gsub('postal', 'physical', museum_addy[i])], museum_addy[,museum_addy[i]]
+        ) # this only catches pretty egregious errors in the DB.
+      }
+      # no need for the physical 'walk in' addresses for the institutes.
+      museumsTEST2 <- museumsTEST2[, grep('postal', colnames(museumsTEST2))]
+    },
 
     museums$contact
   )
@@ -34,28 +46,41 @@ contacts_grabber <- function(x){
   # now we will just drop many institute with missing street addresses. The odds that
   # a user of this package would be shipping to those locations is very low.
   museums <- museums[!is.na(museums$postalStreet),]
-
   return(museums)
+
+  url <- "http://sweetgum.nybg.org/science/api/v1/staff/search?country=U.S.A."
+  people <- jsonlite::fromJSON(url)$data
+
+  people <- cbind(
+    people[,c('code', 'firstName', 'lastName', 'correspondent')],
+    people$contact
+  )
+
+  matches <- merge(
+    x = museums, y = people,
+    by = 'code', all.x = TRUE # left join
+  ) |>
+    as.data.frame()
+  matches[matches==""] <- NA
+
 }
 
 museums <- contacts_grabber()
 
-cnames <- colnames(museums$address)
-
-
-post <- cnames[grep('postal', cnames)]
-cnames[grep('physical', cnames)][i]
-
-for (i in 1:seq_along(post)){
-
-  museums[,post[i]] <- ifelse(
-    is.na(
-      museums[,post[i]]),
-    physicalStreet, museums[,post[i]]
+# these just used for testing while not able to reach wifi.
+cnames <- c('postalStreet', 'postalCity', 'postalState', 'postalZipCode', 'postalCountry')
+cnames <- c(cnames, gsub('postal', 'physical', cnames))
+museums <- setNames(
+  data.frame(
+    cbind(
+      matrix(
+        sample(c(NA, 1), prob = c(0.2, 0.8), size = 100, replace = TRUE), ncol = 5
+      ),
+      matrix(rep(2:6, each = 20), ncol = 5)
     )
-}
+  ),
+  cnames
+)
 
-# now we will just drop many institute with missing street addresses. The odds that
-# a user of this package would be shipping to those locations is very low.
-museumsTEST2 <- museumsTEST2[!is.na(museumsTEST2$postalStreet),]
+
 
