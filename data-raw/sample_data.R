1cf998895f11d0122e2bed5eff648f559df92c69
sagesteppe <reedbenkendorf27@gmail.com>
2025-10-04 14:10:52 -0500

    update collection examples to data entry sheet

--- data-raw/sample_data.R
+++ data-raw/sample_data.R
@@ -1,15 +1,23 @@
 library(usethis)
 library(googlesheets4)
 
-setwd('/media/steppe/hdd/BarnebyLives/data-raw')
+#setwd('/media/steppe/hdd/BarnebyLives/data-raw')
+setwd('~/Documents/assoRted/BarnebyLives/data-raw')
 googledrive::drive_auth("reedbenkendorf27@gmail.com")
+
 # read in data from the sheet to process
-collection_examples <- read_sheet('1iOQBNeGqRJ3yhA-Sujas3xZ2Aw5rFkktUKv3N_e4o8M',
-                        sheet = 'Processed - Examples')
+collection_examples <- read_sheet(
+  '1iOQBNeGqRJ3yhA-Sujas3xZ2Aw5rFkktUKv3N_e4o8M', sheet = 'Data Entry - Examples')
 usethis::use_data(collection_examples, overwrite = TRUE)
 
+
+
+## database templates
 database_templates <- read.csv('Fields.csv')
 usethis::use_data(database_templates, overwrite = TRUE)
+
+
+
 
 
 
